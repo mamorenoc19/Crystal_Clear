eb75668a72ad6851f20068bef7d2d9a456f52b31
Manuela Moreno <mmorenoc2@eafit.edu.co>
2021-10-21 06:39:57 -0500

    Actualización templates
    Actualización completa de templates del home, mejoras de diseño

--- Crystal_Clear_Project/Crystal_Clear_App/urls.py
+++ Crystal_Clear_Project/Crystal_Clear_App/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home)
+    path("", views.home, name="home"),
+    path("login/",views.login, name="login"),
+    path("Sign-in/", views.sign_in, name="sign_in"),
+    path("home_login/", views.home_login, name='home_login'),
+    path("clase/", views.clase, name='clase'),
+    path("crear_clase/", views.crear_clase, name='crear_clase'),
+    path("crear_login/", views.crear_examen, name='crear_examen'),
 ]
--- Crystal_Clear_Project/Crystal_Clear_App/views.py
+++ Crystal_Clear_Project/Crystal_Clear_App/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render
+import random
 
 # Create your views here.
 def home(request):
     return render(request, "Crystal_Clear_App/home.html")
+
+def login(request):
+    return render(request, "Crystal_Clear_App/login.html")
+
+def sign_in(request):
+    return render(request, "Crystal_Clear_App/sign_in.html")
+
+def home_login(request):
+    estudiante = random.choice(lista_estudiante)
+    context={'estudiante':estudiante}
+    return render(request, "Crystal_Clear_App/home_login.html", context)
+
+lista_estudiante = ['Camila', 'alejandra', 'jose miguel']
+
+def clase(request):
+    context = {'estudiante':estudiante}
+    return render(request, "Crystal_Clear_App/clase.html", context)
+
+
+estudiante = [{'nombre': 'camila', 'numero': 1},
+                    {'nombre': 'jose', 'numero': 2}, {'nombre': 'milerna', 'numero': 3}, {'nombre': 'carlos', 'numero': 4}]
+
+def crear_clase(request):
+    return render(request, "Crystal_Clear_App/crear_clase.html")
+
+
+
+def crear_examen(request):
+    return render(request, "Crystal_Clear_App/crear_examen.html")
+
